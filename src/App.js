3c46f6a3307490428d03eaf4e79e935b7296b33e
Mariya <mariya.podosinova@gmail.com>
2020-05-29 15:03:36 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,43 +1,81 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const NUM_BOXES = 31;
+
+const Box = ({ color }) => {
+  const style = {
+    width: "180px",
+    height: "180px",
+    display: "inline-block",
+    backgroundColor: color,
+  };
+  return <div style={style} />;
+};
 
 class App extends Component {
-  
   constructor(props) {
     super(props);
-  }
-  
-  render() {
+    const boxes = Array(NUM_BOXES).fill().map(this.getRandomColor, this);
+    this.state = { boxes };
 
-    return (
-      <div className="App">
-        Render boxes here
-      </div>
-    );
+    setInterval(() => {
+      const boxes = this.state.boxes.slice();
+      const randIndex = Math.floor(Math.random() * boxes.length);
+      boxes[randIndex] = this.getRandomColor();
+      this.setState({ boxes });
+    }, 3000);
+  }
+
+  getRandomColor() {
+    let colorIndex = Math.floor(Math.random() * this.props.allColors.length);
+    return this.props.allColors[colorIndex];
+  }
+
+  render() {
+    const boxes = this.state.boxes.map((color, index) => (
+      <Box key={index} color={color} />
+    ));
+    return <div className="App">{boxes}</div>;
   }
 }
 
 App.defaultProps = {
-  allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
-              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
-              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
-              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
-              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
-              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
-              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
-              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
-              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
-              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
-              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
-              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
-              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
-              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
-              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
-              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
-              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
-              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
-              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
-              "Yellow","YellowGreen"]
+  allColors: [
+    "#000000",
+    "#080808",
+    "#101010",
+    "#181818",
+    "#202020",
+    "#282828",
+    "#303030",
+    "#383838",
+    "#404040",
+    "#484848",
+    "#505050",
+    "#585858",
+    "#606060",
+    "#686868",
+    "#707070",
+    "#787878",
+    "#808080",
+    "#888888",
+    "#909090",
+    "#989898",
+    "#A0A0A0",
+    "#A8A8A8",
+    "#B0B0B0",
+    "#B8B8B8",
+    "#C0C0C0",
+    "#C8C8C8",
+    "#D0D0D0",
+    "#D8D8D8",
+    "#E0E0E0",
+    "#E8E8E8",
+    "#F0F0F0",
+    "#F8F8F8",
+    "#FFFFFF",
+  ],
 };
 
 export default App;
