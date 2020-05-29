d67972e824a9bd0abb87c2eb88ae7e83ec6cbee0
Mariya <mariya.podosinova@gmail.com>
2020-05-29 15:06:50 +0100

    Updates

---
+++ precache-manifest.8640d13fc99de88e6024ecfa29763f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2c50a115ac3a040763ace82fb12ad5",
+    "url": "/50_shades_of_grey/index.html"
+  },
+  {
+    "revision": "d44e27003c2ffded1642",
+    "url": "/50_shades_of_grey/static/css/main.fc5e0f8f.chunk.css"
+  },
+  {
+    "revision": "b2014deb6d21feaff11b",
+    "url": "/50_shades_of_grey/static/js/2.9d02a520.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/50_shades_of_grey/static/js/2.9d02a520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44e27003c2ffded1642",
+    "url": "/50_shades_of_grey/static/js/main.b3956642.chunk.js"
+  },
+  {
+    "revision": "73acd28da92ba5a1619f",
+    "url": "/50_shades_of_grey/static/js/runtime-main.2f38ae7b.js"
+  }
+]);
